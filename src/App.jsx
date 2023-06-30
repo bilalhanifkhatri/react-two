658b2d77ef7e56f2694126e07dae06d5f7f6a5fa
bilalhanifkhatri <bilalhanifkhatri@gmail.com>
2023-07-01 01:43:30 +0500

    After completing Home Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header.jsx';
+import Home from './components/Home.jsx';
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
---
+++ src/components/Home.jsx
@@ -0,0 +1,98 @@
+import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import IMG1 from '../assets/1.jpg';
+import IMG2 from '../assets/2.jpg';
+import IMG3 from '../assets/3.jpg';
+import IMG4 from '../assets/4.jpg';
+import IMG5 from '../assets/5.png';
+
+const headingOptions = {
+  bgColor: 'blackAlpha.600',
+  color: 'white',
+  top: '50%',
+  left: '50%',
+  p: '4',
+  pos: 'absolute',
+  transform: 'translate(-50%,-50%)',
+  textTransform: 'uppercase',
+};
+
+const Home = () => {
+  return (
+    <Box>
+      <MyCarousel />
+
+      <Container maxW={'container.xl'} minH={'100vh'} p={16}>
+        <Heading
+          textTransform={'uppercase'}
+          py={2}
+          borderBottom={'2px solid'}
+          w={'fit-content'}
+          m={'auto'}
+        >
+          Services
+        </Heading>
+        <Stack
+          p={'4'}
+          h={'full'}
+          alignItems={'center'}
+          direction={['column', 'row']}
+        >
+          <Image src={IMG5} h={['40', '400']} filter={'hue-rotate(-130deg)'} />
+          <Text
+            textAlign={'justify'}
+            letterSpacing={["normal",'wider']}
+            lineHeight={['100%', '200%']}
+            padding={['2', '16']}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi id
+            eum vitae natus quibusdam quasi nesciunt, sit vel esse repudiandae
+            dignissimos? Recusandae reiciendis ducimus quasi soluta? Dolore illo
+            quam corporis! Ipsam ducimus voluptatibus dolorem laborum eveniet
+            dolorum excepturi rem voluptates praesentium repellat odio assumenda
+            accusamus commodi laudantium error, nam quos voluptate animi est
+            magni iusto iste cum ex obcaecati. Quasi. Libero aliquid tenetur
+            modi neque commodi, enim eligendi voluptatibus repellendus dolores
+            in quaerat pariatur quia unde illum natus error culpa dolore
+            temporibus assumenda, soluta asperiores fugit. Autem quibusdam
+            dolorem repellendus. Ad dolorum voluptatibus expedita quam
+            consequatur praesentium hic molestias dolorem quas nostrum, sapiente
+            impedit vero sequi obcaecati! Ducimus corrupti debitis aut. Impedit
+            deleniti voluptates repudiandae quaerat, sapiente ex odio earum!
+          </Text>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+const MyCarousel = () => (
+  <Carousel
+    autoPlay
+    infiniteLoop
+    interval={1000}
+    showArrows={false}
+    showStatus={false}
+    showThumbs={false}
+  >
+    <Box w={'full'} h={'100vh'}>
+      <Image src={IMG1} />
+      <Heading {...headingOptions}>Watch the Future</Heading>
+    </Box>
+    <Box w={'full'} h={'100vh'}>
+      <Image src={IMG2} />
+      <Heading {...headingOptions}>Future is Gaming</Heading>
+    </Box>
+    <Box w={'full'} h={'100vh'}>
+      <Image src={IMG3} />
+      <Heading {...headingOptions}>Gaming on Console</Heading>
+    </Box>
+    <Box w={'full'} h={'100vh'}>
+      <Image src={IMG4} />
+      <Heading {...headingOptions}>Night Life is cool</Heading>
+    </Box>
+  </Carousel>
+);
+
+export default Home;
